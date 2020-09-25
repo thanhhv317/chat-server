a6edd3c881596b1443cb353c0396c4bb068b43cd
new-thanh <new-thanh@eplus.vn>
2020-09-25 16:18:41 +0700

    change Models

--- models/Message.js
+++ models/Message.js
@@ -1,27 +1,28 @@
 const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
+const messageSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    message: {
+      type: String,
+      require: true,
+    },
+    room: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "ACTIVE",
+      enum: ["ACTIVE", "DELETE"],
+    },
   },
-  message: {
-    type: String,
-    require: true,
-  },
-  room: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    default: "ACTIVE",
-    enum: ["ACTIVE", "DELETE"],
-  },
-  createAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Message", messageSchema);
--- models/Room.js
+++ models/Room.js
@@ -1,28 +1,29 @@
 const mongoose = require("mongoose");
 
-const roomSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
+const roomSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      default: "",
+    },
+    isLock: {
+      type: String,
+      defaul: "NO",
+      enum: ["YES", "NO"],
+    },
+    status: {
+      type: String,
+      default: "ACTIVE",
+      enum: ["ACTIVE", "DELETE"],
+    },
   },
-  password: {
-    type: String,
-    default: "",
-  },
-  isLock: {
-    type: String,
-    defaul: "NO",
-    enum: ["YES", "NO"],
-  },
-  status: {
-    type: String,
-    default: "ACTIVE",
-    enum: ["ACTIVE", "DELETE"],
-  },
-  createAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Room", roomSchema);
